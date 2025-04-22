5377fb4212b3a54b315c0a38b0b124bcbdec26a8
yrkeviin <kevineziquiellima160@gmail.com>
2025-04-22 16:10:07 -0300

    Feat: Final commit

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './navigation/TabNavigator';
+import KevinNavigator from './navigation/KevinNavigator';
+import NegaoNavigator from './navigation/RaulNavigator';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function App() {
   return (
